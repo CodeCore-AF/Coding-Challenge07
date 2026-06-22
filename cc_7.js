a94271187d7f83a1b2b79078fc0a10faf622ba39
Alessia Foronda <linuxseer@keepmoving.us>
2026-06-21 21:24:45 -0400

    Added functionality to form to allow user to change CTA button's text.

--- cc_7.js
+++ cc_7.js
@@ -1,3 +1,13 @@
 //Use form input and DOM API to 
-//dynamically update CTA (AKA C T A) based on user input
+//dynamically update CTA (AKA Call To Action) based on user input
 //INCOMPLETE
+
+//event listener to pull inputs from CTA form.
+document.getElementById("CTAForm").addEventListener("submit", (event)=>{
+    event.preventDefault();
+    //assigns the input to constant text.
+    const text = document.getElementById("newCTAText").value;
+
+    //Update the CTA button with new text
+    document.getElementById("heroButton").innerHTML = text;
+});
